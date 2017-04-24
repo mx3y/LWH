6e312a60a1608d62580d956f3940e60bf63d74d1
zhengxx-szu <330578655@qq.com>
2017-04-24 18:10:09 +0800

    ...

---
+++ Web/Properties/lwh/js/log_query.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $(".nav-open").bind("click",function(){
+        $(".nav-left").css("left","0px");
+        $(".main").css("left","12%");
+        $(".main").css("width","88%");
+        $(".nav-open").hide();
+        $(".nav-close").show();
+
+    });
+    $(".nav-close").bind("click",function(){
+        $(".nav-left").css("left","-12%");
+        $(".main").css("left","0px");
+        $(".main").css("width","100%");
+        $(".nav-close").hide();
+        $(".nav-open").show();
+    });
+    $.ajax({ 
+        type:"post", 
+        url:"http://192.168.1.168/api/showLog",
+        dataType:"jsonp",
+        jsonp:"callback", 
+        success:function(data){
+            for(item in data){
+                $(".show_log").append("<li>"+data[item]+"</li>");
+            }
+        },
+        error:function(){
+            alert('fail');
+        }
+    }); 
+});
